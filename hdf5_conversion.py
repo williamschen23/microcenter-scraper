8f2a202f0123182053e50894c0c0eb8ec0eb96e9
williamschen23 <chenwill2005@gmail.com>
2024-10-27 20:01:21 -0400

    lower file size

--- hdf5_conversion.py
+++ hdf5_conversion.py
@@ -15,18 +15,16 @@ for name, group in csv.groupby('Name'):
     name = re.sub(r'[^a-zA-Z0-9_]', '_', name)
     part = re.sub(r'[^a-zA-Z0-9_]', '_', part)
     group.rename(columns={'Full Price': 'Full_Price', 'Discounted Price': 'Discounted_Price', 'Saved Price': 'Saved_Price'}, inplace=True)
-    group['Full_Price'] = group['Full_Price'].str.replace('$', '', regex=False).str.replace(',', '', regex=False).astype(float)
-    group['Discounted_Price'] = group['Discounted_Price'].str.replace('$', '', regex=False).str.replace(',', '', regex=False).astype(float)
-    group['Saved_Price'] = group['Saved_Price'].str.replace('$', '', regex=False).str.replace(',', '', regex=False).astype(float)
+    group['Full_Price'] = group['Full_Price'].str.replace('$', '', regex=False).str.replace(',', '', regex=False).astype('float16')
+    group['Discounted_Price'] = group['Discounted_Price'].str.replace('$', '', regex=False).str.replace(',', '', regex=False).astype('float16')
+    group['Saved_Price'] = group['Saved_Price'].str.replace('$', '', regex=False).str.replace(',', '', regex=False).astype('float16')
+    group['Time'] = group['Time'].astype('S36')
     subgroup = group[['Time', 'Full_Price', 'Discounted_Price', 'Saved_Price']]
 
     subgroup.attrs.update({'category': category, 'name': name, 'part_id': part})
     
-    hdf.put(f"{category}/{name}", subgroup, format='table', data_columns=True, index=False)
+    hdf.put(f"{category}/{name}", subgroup, format='table', data_columns=True, index=False, complevel=9, complib='zlib')
     hdf.get_storer(f"{category}/{name}").attrs.part_id = part
 
-hdf.close()
-
-hdf = pd.HDFStore('data.h5')
 hdf.close()
 
