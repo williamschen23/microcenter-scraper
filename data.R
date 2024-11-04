32f01c0ba1e9d1807fa362c0aaea21d94011696a
williamschen23 <chenwill2005@gmail.com>
2024-11-03 20:11:51 -0500

    update r script

--- data.R
+++ data.R
@@ -3,20 +3,18 @@ dat <- read.csv('data.csv')
 
 plotItem <- function(data, name){
   tempdat <- data %>% filter(Name == name)
-  tempdat$Discounted.Price = as.numeric(gsub("\\$", "", tempdat$Discounted.Price))
   tempdat$Time = as.Date(tempdat$Time, "%Y-%m-%d")
   tempdat_clean <- na.omit(tempdat)
-  print(ggplot(data=tempdat_clean, aes(x=Time, y=Discounted.Price)) +
+  print(ggplot(data=tempdat_clean, aes(x=Time, y=Discounted_Price)) +
           geom_point() + 
           theme_classic() + 
           ggtitle(name))
 }
 
-# plotItem(dat,"AMD Ryzen™ 7 7700X, Gigabyte B650 Gaming X AX v2, 32GB DDR5")
+str(dat)
 
+# plotItem(dat,"AMD Ryzen™ 7 7700X, Gigabyte B650 Gaming X AX v2, 32GB DDR5")
 
 for(item in unique(dat$Name)){
   plotItem(dat, item)
 }
-
-str(dat)
